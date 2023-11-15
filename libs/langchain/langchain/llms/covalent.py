0760ff1046e4a7d4965ee4ed677ad4f9de38ae4c
Ara Ghukasyan <ara@agnostiq.ai>
2023-11-15 15:39:00 -0500

    add covalent and covalent cloud modules

---
+++ libs/langchain/langchain/llms/covalent.py
@@ -0,0 +1,247 @@
+import logging
+import subprocess
+import time
+from typing import Any, Callable, Dict, List, Optional, Union
+
+import psutil
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.llms.base import LLM
+from langchain.pydantic_v1 import Extra, Field, root_validator
+
+# Handle case that ``covalent`` is not installed.
+# Prevent import errors when loading this module.
+_COVALENT_SDK_INSTALLED = False
+try:
+    import covalent as ct
+    from covalent._results_manager.result import Result
+    from covalent._workflow.electron import Electron
+    from covalent._workflow.lattice import Lattice
+    from covalent.executor.base import AsyncBaseExecutor, BaseExecutor
+    from covalent_dispatcher._cli.service import _read_pid
+    _COVALENT_SDK_INSTALLED = True
+except ImportError:
+    _ELECTRON_TYPE = Any
+    _LATTICE_TYPE = Any
+    _CT_ASYNC_EXECUTOR_TYPE = Any
+    _CT_EXECUTOR_TYPE = Any
+else:
+    _ELECTRON_TYPE = Electron
+    _LATTICE_TYPE = Lattice
+    _CT_ASYNC_EXECUTOR_TYPE = AsyncBaseExecutor
+    _CT_EXECUTOR_TYPE = BaseExecutor
+
+# Handle case that ``covalent-cloud`` is not installed.
+# Prevent import errors when loading this module.
+try:
+    from covalent_cloud import CloudExecutor
+except ImportError:
+    _CC_EXECUTOR_TYPE = Any
+else:
+    _CC_EXECUTOR_TYPE = CloudExecutor
+
+# Alias for brevity.
+# Union of all valid executor types across Covalent and Covalent Cloud.
+_EXECUTOR_TYPE = Union[str, _CT_ASYNC_EXECUTOR_TYPE,
+                       _CT_EXECUTOR_TYPE, _CC_EXECUTOR_TYPE]
+
+
+logger = logging.getLogger(__name__)
+
+
+def _check_installation():
+    """Check that the Covalent SDK is installed."""
+
+    if not _COVALENT_SDK_INSTALLED:
+        raise RuntimeError(
+            "The Covalent SDK is not installed. To install, run\n\n"
+            "pip install covalent"
+        )
+
+
+class _CovalentBase(LLM):
+    """Base class for ``Covalent`` and ``CovalentCloud``."""
+
+    func: Union[Callable, _ELECTRON_TYPE, _LATTICE_TYPE]
+    """
+    Function, task, or workflow to run with Covalent.
+    Must be callable as ``func(prompt, **kwargs)``.
+    """
+
+    executor: Optional[_EXECUTOR_TYPE] = None
+    """
+    Optional Covalent executor for ``func``.
+    Ignored if ``func`` is a Covalent lattice.
+    """
+
+    lattice_kwargs: Dict[str, Any] = Field(default_factory=dict)
+    """Keyword arguments passed to ``ct.lattice``.
+    Ignored if ``func`` is a Covalent lattice."""
+
+    dispatch_kwargs: Dict[str, Any] = Field(default_factory=dict)
+    """Keyword arguments passed to ``ct.dispatch`` or ``cc.dispatch``."""
+
+    api_key: Optional[str] = None
+    """API key for Covalent Cloud. Required if not set manually with ``cc.save_api_key()``."""
+
+    class Config:
+        """Configuration for this Pydantic object."""
+
+        extra = Extra.forbid
+
+    def _covalent_server_ready(self) -> bool:
+        """Returns True if a local Covalent Server is currently running."""
+
+        _config = ct.get_config()["dispatcher"]
+
+        pid = _read_pid(_config["cache_dir"] + "/ui.pid")
+        server_up = psutil.pid_exists(pid) and pid != -1
+
+        return server_up and _config.get("address") and _config.get("port")
+
+    def _start_covalent(self) -> None:
+        """Start local Covalent server."""
+
+        if self._covalent_server_ready():
+            return
+
+        try:
+            # Start local Covalent server. Suppress output.
+            subprocess.run(
+                "covalent start -d",
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True,
+                check=True,
+            )
+        except subprocess.CalledProcessError as err:
+            raise RuntimeError("Failed to start Covalent.") from err
+
+        while not self._covalent_server_ready():
+            logger.debug("Waiting for Covalent server to start...")
+            time.sleep(1)
+
+    def _to_lattice(self) -> _LATTICE_TYPE:
+        """Transform ``self.func`` into a Covalent lattice."""
+
+        if isinstance(self.func, _LATTICE_TYPE):
+            # Do nothing.
+            return self.func
+
+        if isinstance(self.func, _ELECTRON_TYPE):
+            if self.executor is not None:
+                # Override with user-specified the executor.
+                self.func.set_metadata("executor", self.executor)
+            return ct.lattice(self.func, **self.lattice_kwargs)
+
+        return ct.lattice(ct.electron(self.func))
+
+
+class Covalent(_CovalentBase):
+
+    """Covalent wrapper for LLMs.
+
+    Requires the ``covalent`` Python package:
+        .. code-block:: bash
+
+            pip install covalent
+
+    This wrapper can be used to run either a single function, or an entire
+    Covalent workflow (i.e. a ``Lattice``) across multiple local/cloud backends.
+
+    Example:
+        .. code-block:: python
+
+            # Single function example.
+
+            from langchain.llms import Covalent
+            import covalent as ct
+
+            executor = ct.executor.AWSBatchExecutor()  # e.g., assume defaults configured
+
+            covalent = Covalent(func=my_func, executor=executor)
+
+    Example:
+        .. code-block:: python
+
+            # Covalent workflow example.
+
+            from langchain.llms import Covalent
+            import covalent as ct
+
+            # e.g., assume all defaults configured
+            gcp_executor = ct.executor.GCPExecutor()
+            ssh_executor = ct.executor.SSHExecutor()
+            slurm_executor = ct.executor.SlurmExecutor()
+
+            @ct.electron(executor=gcp_executor)
+            def task(*inputs, flag=True):
+                ...
+
+            @ct.electron(executor=ssh_executor)
+            def other_task(filename, save=True, folder="my-folder"):
+                ...
+
+            @ct.electron(executor=slurm_executor)
+            def onprem_task(**options):
+                ...
+
+            @ct.lattice
+            def covalent_workflow(prompt):
+                # uses tasks defined above
+                ...
+
+            covalent = Covalent(func=covalent_workflow)
+    """
+
+    def __init__(self, **kwargs):
+        _check_installation()
+        super().__init__(**kwargs)
+
+    @root_validator(pre=True)
+    def validate_executor(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        """Make sure the ``executor`` attribute is a Covalent ``BaseExecutor`` instance."""
+
+        executor = values.get("executor")
+
+        # Skip check if no executor is specified.
+        if executor is not None:
+            if not isinstance(executor, _CT_EXECUTOR_TYPE):
+                if not isinstance(executor, str):
+                    _type = type(executor).__name__
+                    raise ValueError(
+                        f"Invalid executor ({_type}) for Covalent."
+                    )
+        return values
+
+    @property
+    def _llm_type(self) -> str:
+        """Return type of LLM."""
+        return "covalent"
+
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        """Run the LLM on the given prompt and input."""
+
+        # Start a local Covalent Server.
+        self._start_covalent()
+
+        # Convert ``self.func`` to a Covalent lattice.
+        lattice = self._to_lattice()
+
+        # Dispatch the lattice.
+        dispatch_id = ct.dispatch(
+            lattice, **self.dispatch_kwargs)(prompt, **kwargs)
+
+        # Request the result...
+        result_obj = ct.get_result(dispatch_id, wait=True)
+        if result_obj.status != Result.COMPLETED:
+            raise RuntimeError(
+                f"Dispatch did not succeed. Returned result object:\n\n{result_obj}"
+            )
+
+        return result_obj.result
---
+++ libs/langchain/langchain/llms/covalent_cloud.py
@@ -0,0 +1,164 @@
+import logging
+from typing import Any, Dict, List, Optional
+
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.pydantic_v1 import root_validator
+
+from .covalent import _CovalentBase, Result
+
+# Handle case that ``covalent-cloud`` is not installed.
+# Prevent import errors when loading this module.
+_COVALENT_CLOUD_SDK_INSTALLED = False
+try:
+    import covalent_cloud as cc
+    from covalent_cloud import CloudExecutor
+    from covalent_cloud.dispatch_management.results_manager import FutureResult
+    _COVALENT_CLOUD_SDK_INSTALLED = True
+except ImportError:
+    _CC_EXECUTOR_TYPE = Any
+    _CC_RESULT_TYPE = Any
+else:
+    _CC_EXECUTOR_TYPE = CloudExecutor
+    _CC_RESULT_TYPE = FutureResult
+
+
+logger = logging.getLogger(__name__)
+
+
+def _check_cloud_installation():
+    """Check that the Covalent SDK is installed."""
+
+    if not _COVALENT_CLOUD_SDK_INSTALLED:
+        raise RuntimeError(
+            "The Covalent Cloud SDK is not installed. To install, run\n\n"
+            "pip install covalent-cloud"
+        )
+
+
+class CovalentCloud(_CovalentBase):
+
+    """Covalent Cloud wrapper for LLMs.
+
+    Requires the ``covalent-cloud`` Python package:
+        .. code-block:: bash
+
+            pip install covalent-cloud
+
+    This wrapper can be used to run either a single function, or an entire
+    Covalent workflow (i.e. a ``Lattice``) on the Covalent Cloud platform.
+
+    Example:
+        .. code-block:: python
+
+            # Single function example.
+
+            from langchain.llms import CovalentCloud
+            import covalent_cloud as cc
+
+            cc.save_api_key(API_KEY)
+            executor = cc.CloudExecutor(env="my-env", num_cpus=12, num_gpus=6)
+            covalent = CovalentCloud(func=my_func, executor=executor)
+
+    Example:
+        .. code-block:: python
+
+            # Covalent Cloud workflow example.
+
+            from langchain.llms import CovalentCloud
+            import covalent as ct
+            import covalent_cloud as cc
+
+            small_executor = cc.CloudExecutor(env="my-env", num_cpus=2)
+            medium_executor = cc.CloudExecutor(env="my-other-env", num_cpus=4)
+            gpu_executor = cc.CloudExecutor(env="my-env", num_cpus=12, num_gpus=6)
+
+            @ct.electron(executor=light_executor)
+            def task(*inputs, flag=True):
+                ...
+
+            @ct.electron(executor=medium_executor)
+            def other_task(filename, save=True, folder="my-folder"):
+                ...
+
+            @ct.electron(executor=gpu_executor)
+            def gpu_task(**options):
+                ...
+
+            @ct.lattice(executor=small_executor, workflow_executor=small_executor)
+            def covalent_workflow(prompt):
+                # uses tasks defined above
+                ...
+
+            covalent = CovalentCloud(func=covalent_workflow, api_key=API_KEY)
+    """
+
+    def __init__(self, **kwargs):
+        _check_cloud_installation()
+        super().__init__(**kwargs)
+
+        if self.api_key is not None:
+            # Update Covalent Cloud API key.
+            cc.save_api_key(self.api_key)
+
+    @root_validator(pre=True)
+    def validate_executor(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        """Make sure the ``executor`` attribute is a Covalent Cloud ``CloudExecutor`` instance."""
+
+        executor = values.get("executor")
+
+        # Skip check if no executor is specified.
+        if executor is not None:
+            if not isinstance(executor, _CC_EXECUTOR_TYPE):
+                _type = type(executor).__name__
+                raise ValueError(
+                    f"Invalid executor ({_type}) for Covalent Cloud."
+                )
+        return values
+
+    @property
+    def _llm_type(self) -> str:
+        "covalent-cloud"
+
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        """Run the LLM on the given prompt and input."""
+
+        # Convert ``self.func`` to a Covalent lattice.
+        lattice = self._to_lattice()
+
+        # Dispatch the lattice.
+        dispatch_id = cc.dispatch(
+            lattice, **self.dispatch_kwargs)(prompt, **kwargs)
+
+        # Request the result...
+        result_obj = cc.get_result(dispatch_id, wait=True)
+
+        if result_obj.status != Result.COMPLETED:
+            # Download available information.
+            debug_attrs = ["result", "inputs", "error", "nodes"]
+            _download_attributes(result_obj, *debug_attrs)
+            raise RuntimeError(
+                f"Dispatch did not succeed. Returned result object:\n\n{result_obj}"
+            )
+
+        # Download result.
+        _download_attributes(result_obj, "result")
+        return result_obj.result.value
+
+
+def _download_attributes(result_obj: _CC_RESULT_TYPE, *names) -> None:
+    """Download select attributes of a ``FutureResult``."""
+
+    for name in names:
+        if name == "nodes":
+            node_outputs = result_obj.get_all_node_outputs()
+            for v in node_outputs.values():
+                v.load()
+        else:
+            attr = getattr(result_obj, name)
+            attr.load()
